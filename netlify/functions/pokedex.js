1788fdc95dc24d69b24d99eb16f760009a1572a4
michellejt <mtongas@gmail.com>
2023-04-06 01:27:12 +0930

    Update pokedex.js

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,8 +1,11 @@
-const fetch = require('node-fetch')
+import fetch from 'node-fetch'
 
 exports.handler = async function (event, context) {
   const eventBody = JSON.parse(event.body)
   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
+
+  //console.log(event)
+ 
 
   const response = await fetch(POKE_API)
   const data = await response.json()
